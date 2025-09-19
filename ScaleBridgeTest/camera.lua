576592afa7519100d9ce4d4951f5dcbc00b07245
xandercreates <69139978+xandercreates@users.noreply.github.com>
2025-09-19 13:33:24 -0400

    Update camera.lua
    localize MyCamera

--- ScaleBridgeTest/camera.lua
+++ ScaleBridgeTest/camera.lua
@@ -1,6 +1,7 @@
 --FoxCamera Configs
 FOXCamera = require("FOXCamera")
-MyCamera = FOXCamera.newCamera(models.model.root.Head.EyePos)
+local MyCamera = FOXCamera.newCamera(models.model.root.Head.EyePos) --can be localized now
 MyCamera.doEyeOffset = true
 MyCamera.distance = 5
 FOXCamera.setCamera(MyCamera)
+
